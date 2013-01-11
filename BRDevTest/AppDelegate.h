7588c51374dc4a6dd4cc338ace09767a25397734
biomorph <ravi@server.com>
2013-01-11 15:31:07 -0700

    Initial Commit

---
+++ BRDevTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BRDevTest
+//
+//  Created by Ravi Alla on 1/11/13.
+//  Copyright (c) 2013 Ravi Alla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
